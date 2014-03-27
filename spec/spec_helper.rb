2932104fd8cb44d66ea7e296022f7d4a7f808ab9
Peter <peterhsaxton@gmail.com>
2014-03-27 11:39:33 +0000

    Added Webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ ENV['RACK_ENV'] = 'test'
 
 require './app'
 
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
