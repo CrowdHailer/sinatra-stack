a67e834dacf91982b1673b3fa17442725d2369d4
Peter <peterhsaxton@gmail.com>
2014-03-27 16:26:52 +0000

    create list elements

--- public/fetchdetails.js
+++ public/fetchdetails.js
@@ -1,11 +1,12 @@
 var stackoverflow_path = 'http://arcane-springs-5149.herokuapp.com/' + gon.stackoverflow_id;
 $.get(stackoverflow_path, function(data) {
   data = data['items'][0];
-  var $list = $('<li>'); 
-  alert(data);
+  var $list = $('<ul>'); 
   console.log(data);
   $.each(data, function(item) {
+    var $element = $.('<li>').html(data[item]);
+    $list.append($element);
     console.log(item);
   });
-  $('#stackoverflow-details-container').append(data);
+  $('#stackoverflow-details-container').append($list);
 });
