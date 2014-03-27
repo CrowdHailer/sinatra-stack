88147d847b8aba2d08f5bbd9baca0c931f633ef9
Peter <peterhsaxton@gmail.com>
2014-03-27 16:33:11 +0000

    Selective data taken from JSON

--- public/fetchdetails.js
+++ public/fetchdetails.js
@@ -1,12 +1,14 @@
 var stackoverflow_path = 'http://arcane-springs-5149.herokuapp.com/' + gon.stackoverflow_id;
 $.get(stackoverflow_path, function(data) {
   data = data['items'][0];
-  var $list = $('<ul>'); 
-  console.log(data);
-  $.each(data, function(item) {
-    var $element = $('<li>').html(data[item]);
-    $list.append($element);
-    console.log(item);
-  });
+  var $list = $('<ul>');
+  $('<li>').html('reputation '+data['reputation']).append_to($list);
+  $('<li>').html('badges gold '+data['badge_counts']['gold']).append_to($list);
+  $('<li>').html('badges silver '+data['badge_counts']['silver']).append_to($list);
+
+  // $.each(data, function(item) {
+  //   var $element = $('<li>').html(data[item]);
+  //   $list.append($element);
+  // });
   $('#stackoverflow-details-container').append($list);
 });
