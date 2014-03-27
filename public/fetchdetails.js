6710db9f116960bcd4c8d7a1ed3560cfb96d7c18
Peter <peterhsaxton@gmail.com>
2014-03-27 16:16:24 +0000

    Console noise

--- public/fetchdetails.js
+++ public/fetchdetails.js
@@ -1,4 +1,6 @@
 var stackoverflow_path = 'http://arcane-springs-5149.herokuapp.com/' + gon.stackoverflow_id;
 $.get(stackoverflow_path, function(data) {
+  alert(data);
+  console.log(data);
   $('#stackoverflow-details-container').append(data);
 });
