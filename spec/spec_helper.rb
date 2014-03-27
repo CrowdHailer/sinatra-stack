3ad247af65aadee6e4c0f94924e037646f6940d4
Peter <peterhsaxton@gmail.com>
2014-03-27 11:38:32 +0000

    Generated empty app class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 ENV['RACK_ENV'] = 'test'
 
+
+require './app'
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
