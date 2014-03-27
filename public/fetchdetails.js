fe2672a7e7e34ffbdb857494a33e3a725e2cd050
Peter <peterhsaxton@gmail.com>
2014-03-27 16:22:19 +0000

    Listy times

--- public/fetchdetails.js
+++ public/fetchdetails.js
@@ -1,6 +1,11 @@
 var stackoverflow_path = 'http://arcane-springs-5149.herokuapp.com/' + gon.stackoverflow_id;
 $.get(stackoverflow_path, function(data) {
+  data = data['items'][0];
+  var $list = $('<li>'); 
   alert(data);
   console.log(data);
+  $.each(data function(item) {
+    console.log(item);
+  });
   $('#stackoverflow-details-container').append(data);
 });
