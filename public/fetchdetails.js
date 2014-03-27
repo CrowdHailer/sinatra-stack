f27b2fb34df5eb75b6397a3788882357db3eb71b
Peter <peterhsaxton@gmail.com>
2014-03-27 16:34:55 +0000

    AppendTo for javascript

--- public/fetchdetails.js
+++ public/fetchdetails.js
@@ -2,9 +2,9 @@ var stackoverflow_path = 'http://arcane-springs-5149.herokuapp.com/' + gon.stack
 $.get(stackoverflow_path, function(data) {
   data = data['items'][0];
   var $list = $('<ul>');
-  $('<li>').html('reputation '+data['reputation']).append_to($list);
-  $('<li>').html('badges gold '+data['badge_counts']['gold']).append_to($list);
-  $('<li>').html('badges silver '+data['badge_counts']['silver']).append_to($list);
+  $('<li>').html('reputation '+data['reputation']).appendTo($list);
+  $('<li>').html('badges gold '+data['badge_counts']['gold']).appendTo($list);
+  $('<li>').html('badges silver '+data['badge_counts']['silver']).appendTo($list);
 
   // $.each(data, function(item) {
   //   var $element = $('<li>').html(data[item]);
