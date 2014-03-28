0f33ab78c581dbfdb073e6a938706a59da9036cf
Peter Saxton <peterhsaxton@gmail.com>
2014-03-28 11:25:41 +0000

    removed location

--- public/fetchdetails.js
+++ public/fetchdetails.js
@@ -3,7 +3,7 @@ $.get(stackoverflow_path, function(data) {
   data = data['items'][0];
   var $list = $('<ul>').addClass('inline-list'); 
   console.log(data);
-	$('<li>').html('Location: '+data['location']).appendTo($list);
+	$('<li>').html('Stackoverflow').appendTo($list);
 	$('<li>').html('Reputation: '+data['reputation']).appendTo($list);
 	$('<li>').html('Gold Badges: '+data['badge_counts']['gold']).appendTo($list);
 	$('<li>').html('Silver Badges: '+data['badge_counts']['silver']).appendTo($list);
